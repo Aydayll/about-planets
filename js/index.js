225b70b3f0060bf24a6dc422febcdedb4eca4143
Aydayll <makenovaay23@gmail.com>
2020-12-01 14:32:47 +0600

    add the let slider in js and style the swiper

--- js/index.js
+++ js/index.js
@@ -1,2 +1,27 @@
-var scene = document.getElementById("scene");
-var parallaxInstance = new Parallax(scene);
+let scene = document.getElementById("scene");
+let parallaxInstance = new Parallax(scene);
+
+let keys = [
+    "Mercury",
+    "Venus",
+    "Earth",
+    "Mars",
+    "Jupiter",
+    "Saturn",
+    "Uranus",
+    "Neptune",
+];
+let slider = new Swiper(".swiper-container", {
+  slidesPreView : "auto",
+  spaceBetween: 150,
+  centeredSlides: true,
+  mousewheel: true,
+  pagination:{
+      el:"planet-links",
+      clickable: true,
+      renderBullet: function(index, className){
+          return 'div class="' + className + '">' + keys[index] + "</div>";
+      }   
+  }
+});
+
